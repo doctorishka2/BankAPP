fb2056fc970b0653a0c45878c2c0f114f44c584c
g67oleg@gmail.com <kharkov67>
2024-05-30 18:12:31 -0400

    Adding new TestClassssssssssssss

--- src/test/java/BankAppMainTest.java
+++ src/test/java/BankAppMainTest.java
@@ -4,6 +4,8 @@ public class BankAppMainTest {
     public static void newTesting() {
 
         System.out.println("Test");
+        System.out.println("Test");
+        System.out.println("Test");
 
     }
 
